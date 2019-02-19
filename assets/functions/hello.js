f915efff04cd7afde84ecd6a2522c7933e1f879a
nobii <fujisawa-shin@kayac.com>
2019-02-19 12:52:15 +0900

    npmモジュールをくみこむサンプル

--- assets/functions/hello.js
+++ assets/functions/hello.js
@@ -1,6 +1,8 @@
+const uuidv1 = require('uuid/v1');
+
 exports.handler = (event, context, callback) => {
   callback(null, {
     statusCode: 200,
-    body: "Hello World!",
+    body: uuidv1(),
   })
 }
